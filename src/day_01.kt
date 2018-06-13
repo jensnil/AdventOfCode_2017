541913041110153104763025a08d167fb8a40241
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-13 21:46:43 +0200

    Initial commit

--- src/day_01.kt
+++ src/day_01.kt
@@ -5,5 +5,7 @@ fun main(args: Array<String>) {
     var hej = listOf<Int>(1,2,3)//.toMutableList()
     var c1 = hej.circular()
     //c1.add(4)
+    println(c1[0])
     println(c1[-1])
+    println(c1[1])
 }
