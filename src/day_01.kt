3d7a956c57db53b36dfe1457b342d65d7228d2eb
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-13 21:34:07 +0200

    Initial commit

---
+++ src/day_01.kt
@@ -0,0 +1,9 @@
+import lib.circular
+import lib.swap
+
+fun main(args: Array<String>) {
+    var hej = listOf<Int>(1,2,3)//.toMutableList()
+    var c1 = hej.circular()
+    //c1.add(4)
+    println(c1[-1])
+}
