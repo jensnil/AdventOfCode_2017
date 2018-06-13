dfe8e5c6039bef47167d8e4c7e9bb7e623832c6e
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-13 23:16:15 +0200

    Added Maven files and kt-files

---
+++ src/main/java/day_01.kt
@@ -0,0 +1,11 @@
+import lib.circular
+import lib.swap
+
+fun main(args: Array<String>) {
+    var hej = listOf<Int>(1, 2, 3)//.toMutableList()
+    var c1 = hej.circular()
+    //c1.add(4)
+    println(c1[0])
+    println(c1[-1])
+    println(c1[1])
+}
