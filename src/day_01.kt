c7ab1c1150d73c2f9482f96d11358361ddadbeba
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-13 22:14:53 +0200

    Initial commit

--- src/day_01.kt
+++ src/day_01.kt
@@ -2,7 +2,7 @@ import lib.circular
 import lib.swap
 
 fun main(args: Array<String>) {
-    var hej = listOf<Int>(1,2,3)//.toMutableList()
+    var hej = listOf<Int>(1, 2, 3)//.toMutableList()
     var c1 = hej.circular()
     //c1.add(4)
     println(c1[0])
