57f52bda535054f0d393f502fe4c71a73f8742d7
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-16 01:33:47 +0200

    Squashed commit of the following:
    commit 59cae5c10e80b7b9115e2d803240060c5f0d3975
    Author: Jens Nilsson <unconfigured@null.spigotmc.org>
    Date:   Thu Jun 14 23:08:08 2018 +0200
    
        Added unit tests to day_01

--- src/main/java/day_01/day_01.kt
+++ src/main/java/day_01/day_01.kt
@@ -1,10 +1,5 @@
 package day_01
 
-import lib.circular
-import lib.swap
-import input
-import input_test
-
 fun part_one(inputCircular : List<Int>) : Int {
     return inputCircular.reduceIndexed { index, acc, _ ->
         if (inputCircular[index] == inputCircular[index - 1])
--- src/test/java/day_02/Day_02KtTest.kt
+++ src/test/java/day_02/Day_02KtTest.kt
@@ -1,20 +1,17 @@
-package day_01
+package day_02
 
-import input
-import lib.circular
-
-internal class Day_01KtTest {
-    private val inputCircular = input.toList().map{it.toString().toInt()}.circular()
+internal class Day_02KtTest {
+    private val spreadSheet = input_02.lines().map { it.split('\t').map{ it2 -> it2.toInt()} }
 
     @org.junit.jupiter.api.Test
     fun part_one() {
-        val answer = part_one(inputCircular)
-        assert(answer== 1341)
+        val answer = part_one(spreadSheet)
+        assert(answer== 36766)
     }
 
     @org.junit.jupiter.api.Test
     fun part_two() {
-        val answer = part_two(inputCircular)
-        assert(answer== 1348)
+        val answer = part_two(spreadSheet)
+        assert(answer== 261)
     }
 }
