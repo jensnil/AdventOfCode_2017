f79713bf4db7042c0ca8f8750127ee60415f0958
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-15 21:08:31 +0200

    Squashed commit of the following:
    commit 59cae5c10e80b7b9115e2d803240060c5f0d3975
    Author: Jens Nilsson <unconfigured@null.spigotmc.org>
    Date:   Thu Jun 14 23:08:08 2018 +0200
    
        Added unit tests to day_01

--- src/main/java/day_01/day_01.kt
+++ src/main/java/day_01/day_01.kt
@@ -5,31 +5,20 @@ import lib.swap
 import input
 import input_test
 
-fun part_one(inputCircular : List<Int>) {
-    var count = 0
-    count = inputCircular.reduceIndexed { index, acc, _ ->
+fun part_one(inputCircular : List<Int>) : Int {
+    return inputCircular.reduceIndexed { index, acc, _ ->
         if (inputCircular[index] == inputCircular[index - 1])
             acc + inputCircular[index].toString().toInt()
         else
             acc
     }
-    println(count)
 }
 
-fun part_two(inputCircular : List<Int>) {
-    var count = 0
-    count = inputCircular.reduceIndexed { index, acc, _ ->
+fun part_two(inputCircular : List<Int>) : Int {
+    return inputCircular.reduceIndexed { index, acc, _ ->
         if (inputCircular[index] == inputCircular[index + inputCircular.size/2])
             acc + inputCircular[index].toString().toInt()
         else
             acc
     }
-    println(count)
 }
-
-fun main(args: Array<String>) {
-    val inputCircular = input.toList().map{it.toString().toInt()}.circular()
-    part_one(inputCircular)
-    part_two(inputCircular)
-}
-
