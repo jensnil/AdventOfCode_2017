5e92e753166a72ec285be4861ee7a7decbdadfb6
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-17 22:22:24 +0200

    Solved day 05

--- src/main/kotlin/day_05/day_05.kt
+++ src/main/kotlin/day_05/day_05.kt
@@ -1,10 +1,30 @@
 package day_05
 
 
-fun part_one(input: List<List<String>>): Int {
-    return 0
+fun part_one(input: List<Int>): Int {
+    val instruction = input.toMutableList()
+    var ip = 0
+    var count = 0
+    var tmp: Int
+    while (0 <= ip && ip < input.size) {
+        tmp = instruction[ip]
+        instruction[ip]++
+        ip += tmp
+        count++
+    }
+    return count
 }
 
-fun part_two(input: List<List<String>>): Int {
-    return 0
+fun part_two(input: List<Int>): Int {
+    val instruction = input.toMutableList()
+    var ip = 0
+    var count = 0
+    var tmp: Int
+    while (0 <= ip && ip < input.size) {
+        tmp = instruction[ip]
+        instruction[ip] += if (instruction[ip] >= 3) -1 else 1
+        ip += tmp
+        count++
+    }
+    return count
 }
--- src/test/kotlin/day_05/Day_05KtTest.kt
+++ src/test/kotlin/day_05/Day_05KtTest.kt
@@ -3,9 +3,13 @@ package day_05
 internal class Day_05KtTest {
     @org.junit.jupiter.api.Test
     fun part_one() {
+        val input = input_05.lines().map { it.toInt() }
+        assert(part_one(input) == 339351)
     }
 
     @org.junit.jupiter.api.Test
     fun part_two() {
+        val input = input_05.lines().map { it.toInt() }
+        assert(part_two(input) == 24315397)
     }
 }
