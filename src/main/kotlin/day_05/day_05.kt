904ea77802e2acc0b68f11584644117d7fabbebb
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-06-17 21:04:37 +0200

    Solved day 04

---
+++ src/main/kotlin/day_05/day_05.kt
@@ -0,0 +1,10 @@
+package day_05
+
+
+fun part_one(input: List<List<String>>): Int {
+    return 0
+}
+
+fun part_two(input: List<List<String>>): Int {
+    return 0
+}
---
+++ src/test/kotlin/day_05/Day_05KtTest.kt
@@ -0,0 +1,11 @@
+package day_05
+
+internal class Day_05KtTest {
+    @org.junit.jupiter.api.Test
+    fun part_one() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun part_two() {
+    }
+}
