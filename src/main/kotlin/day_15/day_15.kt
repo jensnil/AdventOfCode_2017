90c9066df6e550905b8b8d8fbd66f02e4c1acaf4
Jens Nilsson <unconfigured@null.spigotmc.org>
2018-07-01 18:51:21 +0200

    Solved Day 15

---
+++ src/main/kotlin/day_15/day_15.kt
@@ -0,0 +1,38 @@
+package day_15
+
+import lib.knothash
+import java.math.BigInteger
+
+val gridSize = 128
+
+fun part_one(factorA: Int, factorB: Int): Int {
+    var matchCount = 0
+    var generatorA : Long = factorA.toLong()
+    var generatorB : Long = factorB.toLong()
+    for (i in 0..40000000) {
+        generatorA = (16807 * generatorA) % 2147483647
+        generatorB = (48271 * generatorB) % 2147483647
+        if (generatorA and 0xFFFF == generatorB and 0xFFFF) {
+            matchCount++
+        }
+    }
+    return matchCount
+}
+
+fun part_two(factorA: Int, factorB: Int) : Int {
+    var matchCount = 0
+    var generatorA : Long = factorA.toLong()
+    var generatorB : Long = factorB.toLong()
+    for (i in 0..5000000) {
+        do {
+            generatorA = (16807 * generatorA) % 2147483647
+        } while (generatorA % 4L != 0L);
+        do {
+            generatorB = (48271 * generatorB) % 2147483647
+        } while (generatorB % 8L != 0L);
+        if (generatorA and 0xFFFF == generatorB and 0xFFFF) {
+            matchCount++
+        }
+    }
+    return matchCount
+}
